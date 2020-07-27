2b6643496b2b37a0c54c0f445b9f436f62d7c4f9
htutich <51759986+htutich@users.noreply.github.com>
2020-07-27 22:41:31 +0300

    Add files via upload

---
+++ ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            var lname = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Введите рост");
+            var hman = Console.ReadLine();
+
+            Console.WriteLine("Введите вес");
+            var lman = Console.ReadLine();
+
+
+            Console.WriteLine(name + " " + lname + " " + age + " " + hman + " " + lman);
+            Console.WriteLine("{0} {1} {2} {3} {4}", name, lname, age, hman, lman);
+            Console.WriteLine($"{name} {lname} {age} {hman} {lman}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 400;
+            int a2 = 500;
+            Console.WriteLine($" {a1} {a2}");
+            int a3 = a1;
+            a1 = a2;
+            a2 = a3;
+            Console.WriteLine($" {a1} {a2}");
+            Console.WriteLine("\n\n\n\n");
+
+
+
+
+            string b1 = "fsdfsdf";
+            string b2 = "c333";
+            Console.WriteLine($" {b1} {b2}");
+
+            (b1, b2) = (b2, b1);
+            Console.WriteLine($" {b1} {b2}");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
