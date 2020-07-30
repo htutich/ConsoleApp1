911686b9e399d0a6c0f71c5e134fabf917d5c1cb
htutich <fee.123@yandex.ru>
2020-07-30 22:00:42 +0300

    второй урок

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Написать метод, возвращающий минимальное из трёх чисел.
+Алексеев Артём
+*/
+namespace ex1
+{
+    class Program
+    {
+
+        static double minNumber(params int[] numbers)
+        {
+            double min = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                min = (min > numbers[i]) ? numbers[i] : min;
+            }
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            double min = minNumber(3, 5, 7, 3, 4, 7, 1);
+            Console.WriteLine($"{min:F2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+Алексеев Артём
+*/
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool doit = true;
+            int num = 0;
+            Console.WriteLine("С клавиатуры вводятся числа, пока не будет введен 0. После подсчет суммы всех нечетных положительных чисел.");
+            
+            while (doit)
+            {
+                string str = Console.ReadLine();
+                int nnm = Convert.ToInt32(str);
+
+                if (nnm == 0) {
+                    doit = false;
+                    break;
+                }
+                num += (nnm % 2 == 0) ? ((nnm > 0) ? nnm : 0) : 0;
+
+            }
+
+            Console.WriteLine($"Сумма: {num}");
+            Console.ReadLine();
+        }
+    }
+}
