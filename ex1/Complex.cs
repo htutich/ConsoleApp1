32fd82c71963183b2c181244c8c069d49e653d33
htutich <fee.123@yandex.ru>
2020-08-03 21:36:07 +0300

    upload

---
+++ ex1/Complex.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ex1
+{
+    class Complex
+    {
+        // Поля приватные.
+        private double im;
+        // По умолчанию элементы приватные, поэтому private можно не писать.
+        double re;
+
+        // Конструктор без параметров.
+        public Complex()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        // Конструктор, в котором задаем поля.    
+        // Специально создадим параметр re, совпадающий с именем поля в классе.
+        public Complex(double _im, double re)
+        {
+            // Здесь имена не совпадают, и компилятор легко понимает, что чем является.              
+            im = _im;
+            // Чтобы показать, что к полю нашего класса присваивается параметр,
+            // используется ключевое слово this
+            // Поле параметр
+            this.re = re;
+        }
+
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+
+        public Complex Multiply(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im * im;
+            x3.re = x2.re * re;
+            return x3;
+        }
+
+
+        // Свойства - это механизм доступа к данным класса.
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                // Для примера ограничимся только положительными числами.
+                if (value >= 0) im = value;
+            }
+        }
+        // Специальный метод, который возвращает строковое представление данных.
+        public new string ToString()
+        {
+            return $"\n{re} + {im}i";
+        }
+    }
+
+}
---
+++ ex1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+//а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.
+//б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+//в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
+//Алексеев Артём
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод значений для комплексных чисел
+            Console.Write("Введите реальное(re) значение первого комплексного числа: ");
+            int userComplexReFirst = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите мнимое(im) значение первого комплексного числа: ");
+            int userComplexImFirst = Convert.ToInt32(Console.ReadLine());
+
+            Complex userComplex1 = new Complex(userComplexImFirst, userComplexReFirst);
+
+            Console.Write("Введите реальное(re) значение второго комплексного числа: ");
+            int userComplexReSecond = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите мнимое(im) значение второго комплексного числа: ");
+            int userComplexImSecond = Convert.ToInt32(Console.ReadLine());
+
+            Complex userComplex2 = new Complex(userComplexImSecond, userComplexReSecond);
+
+            Console.WriteLine("Пожалуйста, нажмите на выбор действие(с цифровой клавиатуры), которое хотите произвести с данными комплексными числами: \n \"+\" - сложить комплексные числа;\n \"*\" - перемножить комплексные числа;\n \"-\" - вычесть комплексные числа;");
+            
+            //Запоминаем кнопку, которую нажал пользователь
+            ConsoleKeyInfo choose = Console.ReadKey();
+            
+            switch (choose.Key)
+            {
+
+                // умножение.
+                case ConsoleKey.Multiply: Console.WriteLine(userComplex1.Multiply(userComplex2).ToString()); break;
+
+                // сложение
+                case ConsoleKey.Add: Console.WriteLine(userComplex1.Plus(userComplex2).ToString()); break;
+
+                // вычитание
+                case ConsoleKey.Subtract:
+                    Console.WriteLine("\nПожалуйста, нажмите на выбор, какое из какого числа хотите вычесть: \n1. Второе из первого.\n2. Первое из второго;");
+                    choose = Console.ReadKey();
+
+                    //Проверяем, какую цифру с клавиатуры нажали
+                    if (choose.Key == ConsoleKey.NumPad1) Console.WriteLine(userComplex1.Minus(userComplex2).ToString());
+                    else if (choose.Key == ConsoleKey.NumPad2) Console.WriteLine(userComplex2.Minus(userComplex1).ToString());
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
